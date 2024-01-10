1bf47d1dae9ba26d9cd8de0e65cf396ec59dfa72
Vova1982 <64212894+Vova1982@users.noreply.github.com>
2024-01-09 21:35:48 -0500

    add forms

--- script.js
+++ script.js
@@ -18,3 +18,27 @@ document.querySelector("#inp3").oninput = () => {
     // console.log(document.querySelector('#inp3').value);
     document.querySelector('.spn3').innerHTML = document.querySelector('#inp3').value;
 }
+
+document.querySelector('#btn_1').onclick = () => {
+    console.log(document.querySelector('#i2').value);
+}
+
+document.querySelector('#btn_1').onclick = () => {
+    let One = document.querySelector('#i2');
+    console.log(One.checked);
+    if (One.checked) { 
+        console.log("Hello its great")
+    } else {
+        console.log("Check onr more time")
+    }
+}
+
+document.querySelector('#btn_4').onclick = () => { 
+    console.log(document.querySelector('#text_1').value);
+}
+
+document.querySelector('#btn_4').onclick = (event) => {
+    event.preventDefault();
+    console.log(document.querySelector('#text_2').value);
+        console.log(document.querySelector('#text_1').value)
+}
